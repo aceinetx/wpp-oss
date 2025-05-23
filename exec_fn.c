5851144e3fdb3c96d66b693a45b9ad40edb78889
aceinetx <mouselin82@gmail.com>
2025-05-23 12:13:33 +0300

    Advanced call syntax
    Derived from watermelon++ v1.1.1

--- exec_fn.c
+++ exec_fn.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *strdup (const char *);
+
 bool
 wpp_do_nf (wppExec *exec)
 {
@@ -57,10 +59,64 @@ bool
 wpp_do_call (wppExec *exec)
 {
   wppToken name;
+  wppObject arg_var;
+  unsigned int arg_index;
+  char arg_name[32];
+
+  arg_index = 1;
 
   name = wpp_lexer_next (exec->lexer);
   DO_TEST_TOKEN (name, WPP_TOKEN_IDENTIFIER);
-  DO_TEST_TOKEN (wpp_lexer_next (exec->lexer), WPP_TOKEN_SEMICOLON);
+
+  for (;;)
+    {
+      wppToken tok;
+      tok = wpp_lexer_next (exec->lexer);
+
+      if (tok.type == WPP_TOKEN_SEMICOLON)
+        break;
+
+      sprintf (arg_name, "arg%u", arg_index);
+      arg_var.name = strdup (arg_name);
+      wpp_arena_append (&exec->strings_arena, arg_var.name);
+
+      if (tok.type == WPP_TOKEN_INT)
+        {
+          arg_var.type = WPP_OBJ_INT;
+          arg_var.as._int = tok.as.number;
+        }
+      else if (tok.type == WPP_TOKEN_FLOAT)
+        {
+          arg_var.type = WPP_OBJ_FLOAT;
+          arg_var.as._float = tok.as.fnumber;
+        }
+      else if (tok.type == WPP_TOKEN_STRING)
+        {
+          arg_var.type = WPP_OBJ_STRING;
+          arg_var.as.string = tok.as.str;
+        }
+      else if (tok.type == WPP_TOKEN_IDENTIFIER)
+        {
+          wppObject *var;
+          var = wpp_exec_getvar (exec, tok.as.str);
+          if (!var)
+            {
+              sprintf (exec->error, "call: undefined variable");
+              return false;
+            }
+
+          wpp_exec_obj_eq (exec, &arg_var, var);
+        }
+      else
+        {
+          sprintf (exec->error, "call: invalid token");
+          return false;
+        }
+
+      wpp_exec_assign (exec, arg_name, arg_var);
+
+      arg_index++;
+    }
 
   if (wpp_exec_fcall (exec, name.as.str) == FCALL_FAIL)
     return false;
